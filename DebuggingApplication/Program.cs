fc00d0f9ab82becd73707a35ccf8bb95d9701ed2
Jay Koshti <jay.koshti@simformsolutions.com>
2023-05-15 13:18:12 +0530

    Added Debugging Application

---
+++ DebuggingApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string? userResponse = Console.ReadLine();
+
+        if (Convert.ToBoolean(int.Parse(userResponse)))
+        {
+            Console.WriteLine("The entered value is a integer");
+        }
+    }
+}
