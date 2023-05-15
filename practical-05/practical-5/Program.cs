053c76c49c588cf8db68a4fa6e713b4c62363cc6
Jay Koshti <jay.koshti@simformsolutions.com>
2023-05-15 13:33:32 +0530

    Added Practical-5

---
+++ practical-05/practical-5/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+                
+        try
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"\nValue at index {i} is {arr[i]}");
+            }
+        }
+        catch( IndexOutOfRangeException e )
+        {
+            Console.WriteLine($"\n{e.Message}");
+        }
+        finally
+        {
+            Console.WriteLine($"\nThis is finally block");
+        }          
+    }
+}
