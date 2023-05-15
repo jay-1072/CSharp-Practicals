3edac3b47f1c69db0ead2981fe0ff4df984c95d2
Jay Koshti <jay.koshti@simformsolutions.com>
2023-05-15 13:37:54 +0530

    Added Practical-6

---
+++ practical-06/practical-6/ProcessBuissnessLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_6
+{
+    public class ProcessBuissnessLogic
+    {
+        public delegate void ProcessCompletedEventHandler(object source, EventArgs e);
+
+        public event ProcessCompletedEventHandler? ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+            Console.WriteLine("\nWork in progrss...");
+            Thread.Sleep(3000);
+            Console.WriteLine("\nProcess Completed");
+            OnProcessCompleted();
+        }
+
+        protected virtual void OnProcessCompleted()
+        {            
+            ProcessCompleted?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ practical-06/practical-6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using practical_6;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ProcessBuissnessLogic processBuissnessLogic = new ProcessBuissnessLogic();
+        processBuissnessLogic.ProcessCompleted += bl_ProcessCompleted;
+
+        processBuissnessLogic.StartProcess();        
+    }
+
+    public static void bl_ProcessCompleted(object source, EventArgs e)
+    {
+        Console.WriteLine($"\nMethod Invoked");
+    }
+}
