e47353fd19de42909de941c24f2557c501438c96
Jay Koshti <jay.koshti@simformsolutions.com>
2023-05-15 13:23:49 +0530

    Added Practical-2

---
+++ Practical-02/practical-2/Customer_Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace practical_2
+{
+    public class Customer_Account
+    {
+        public string? bank_name;
+        public long customer_accountNo;
+        public string? customer_name;
+
+        public Customer_Account(long accountNo, string customerName)
+        {
+            customer_accountNo = accountNo;
+            customer_name = customerName;
+        }
+
+        /* PrintInfo */
+        // This method will print the customer account details
+        // Return type of this method is void
+        public void PrintInfo()
+        {
+            Console.WriteLine($"\nBank name\t\tAccount number\t\tCustomer name");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\n{this.bank_name}\t{this.customer_accountNo}\t\t{this.customer_name}");
+            Console.ForegroundColor= ConsoleColor.White;
+        }
+    }
+}
---
+++ Practical-02/practical-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using practical_2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Customer_Account customerAccount1 = new Customer_Account(190170116026, "Jay Koshti");
+        customerAccount1.bank_name = "State Bank Of India";
+
+        customerAccount1.PrintInfo();
+
+        Console.ReadLine();
+    }
+}
