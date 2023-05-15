ebe6c910fe99368dd370f12ac47a1e293f5813ed
Jay Koshti <jay.koshti@simformsolutions.com>
2023-05-15 13:15:01 +0530

    Added Practical-1

---
+++ Practical-01/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace practical_1
+{
+    public class Calculator
+    {
+        public static void Add(decimal x, decimal y)
+        {         
+            Console.WriteLine($"\nThe sum of the {x} and {y} is {Math.Round(x + y, 2)}");
+        }
+
+        public static void Subtract(decimal x, decimal y)
+        {
+            Console.WriteLine($"\nThe subtraction of the {x} and {y} is {Math.Round(x - y, 2)}");
+        }
+
+        public static void Multiply(decimal x, decimal y)
+        {
+            Console.WriteLine($"\nThe multiplication of the {x} and {y} is {Math.Round(x * y, 2)}");
+        }
+
+        public static void Divide(decimal x, decimal y)
+        {
+            Console.WriteLine($"\nThe division of the {x} and {y} is {Math.Round(x / y, 2)}");            
+        }
+    }
+}
---
+++ Practical-01/Program.cs
@@ -0,0 +1,51 @@
+﻿using practical_1;
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("\nEnter first number : ");
+            decimal number1;
+            bool IsNumber1ConversionSuccessfull = decimal.TryParse(Console.ReadLine(), out number1);
+
+            if (IsNumber1ConversionSuccessfull)
+            {
+                Console.Write("\nEnter second number : ");
+                decimal number2;
+                bool IsNumber2ConversionSuccessfull = decimal.TryParse(Console.ReadLine(), out number2);
+
+                if (IsNumber2ConversionSuccessfull)
+                {
+                    Calculator.Add( number1, number2 );
+                    Calculator.Subtract( number1, number2 );
+                    Calculator.Multiply( number1, number2 );
+                    
+                    if (number2 == 0)
+                    {                        
+                        Console.WriteLine($"\nCannot divide {number1} by {number2}");                        
+                    }
+                    else
+                    {
+                        Calculator.Divide( number1, number2 );
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"\nThe number must be between {decimal.MinValue} and {decimal.MaxValue}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"\nThe number must be between {decimal.MinValue} and {decimal.MaxValue}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"\n{e.Message}");
+        }
+
+        Console.ReadLine();
+    }
+}
